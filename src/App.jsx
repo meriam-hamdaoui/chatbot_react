c9ecaa9d832d4426a457efebf305294922c0413b
meriam <meriam.hamdaoui@gmail.com>
2025-07-26 17:10:32 +0100

    set up the ui layout and fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-import "./App.css";
+// npm packages
+import { useState } from "react";
+// components
+import Chat from "./components/Chatbot/Chat";
+// data
+import { MESSAGES } from "./const/Messages";
+import robot from "./assets/chatbot.png";
+// styles
+import styles from "./App.module.css";
 
 export default function App() {
-  return <>Hello I'm you chatbot friend. How can I help you today.</>;
+  const [messages, setMessages] = useState(MESSAGES);
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.Header}>
+        <img className={styles.Logo} src={robot} />
+        <h2 className={styles.Title}>AI Chatbot</h2>
+      </header>
+      <div className={styles.ChatContainer}>
+        <Chat messages={messages} />
+      </div>
+    </div>
+  );
 }
